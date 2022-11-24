ef35d5261bb4256d732ca3af7a12a6e5a25b31fc
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-24 14:38:04 -0600

    agregar librería angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-empleados-it',
+    appId: '1:1091865789621:web:d7d0ac1beeeefa155d2afc',
+    storageBucket: 'angular-empleados-it.appspot.com',
+    apiKey: 'AIzaSyCNUPDcI0KU9ARVnExoF0qrJrVzH9gF2LI',
+    authDomain: 'angular-empleados-it.firebaseapp.com',
+    messagingSenderId: '1091865789621',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular-empleados-it',
+    appId: '1:1091865789621:web:d7d0ac1beeeefa155d2afc',
+    storageBucket: 'angular-empleados-it.appspot.com',
+    apiKey: 'AIzaSyCNUPDcI0KU9ARVnExoF0qrJrVzH9gF2LI',
+    authDomain: 'angular-empleados-it.firebaseapp.com',
+    messagingSenderId: '1091865789621',
+  },
   production: false
 };
 
