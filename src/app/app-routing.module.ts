4ae87e83183f582acd0352495cb3db491939a69e
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-26 16:10:08 -0600

    proteger rutas privadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
 import { PlacesComponent } from './components/places/places.component';
 import { LoginComponent } from './auth/login/login.component';
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard'
 
 const routes: Routes = [
-  { path: 'places', component: PlacesComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  {
+    path: 'places',
+    component: PlacesComponent,
+    // Nueva API de Angular Fire (ya contiene guards para saber si un usuario esta o no autenticado)
+    ...canActivate(() => redirectUnauthorizedTo('/login'))
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    // Redireccionar a un usuario autenticado a la ruta principal de la aplicación
+    ...canActivate(() => redirectLoggedInTo('/'))
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    ...canActivate(() => redirectLoggedInTo('/'))
+  },
+  { path: '', redirectTo: 'places', pathMatch: 'full' }
 ];
 
 @NgModule({
